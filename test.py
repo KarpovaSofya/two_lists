a606094ca447018ae2d34e7947ca2535a2cad550
KarpovaSofya <karpova.sofya@yandex.ru>
2019-05-02 13:16:00 +0300

    Add files via upload

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import unittest
 import json
 from stag import func_sorted
 from stag import func1
+from stag import func_bad
 
 with open('data_db.json') as f_obj:
 	data = json.load(f_obj)
@@ -25,6 +26,12 @@ class TestUM(unittest.TestCase):
 	def test_sorted(self):
 		for i in range(len(data)):
 			self.assertEqual(func_sorted(main_lst,data[i][0]),data[i][-1])
+# test и test_sorted в качестве второго аргумента принимает уже посчитанное с помощью функции func_bad значение для того, чтобы сократить время тестирования.
+# Можно использовать тест, где сравнение будет происходить с результатом функции func_bad (значение высчитывается прямо в теле теста).
+
+	def test_main(self):
+		for i in range(len(data)):
+			self.assertEqual(func_sorted(main_lst,data[i][0]),func_bad(main_lst,data[i][0]))
 
 	def test_equal(self):
 		for i in range(len(data)):
